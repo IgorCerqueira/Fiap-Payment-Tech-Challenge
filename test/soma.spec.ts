da7685b31cd879675174ee0f0b255bf02a7f100a
Igor Oliveira <igoro.cerqueira@outlook.com>
2024-01-28 01:56:31 -0300

    coverage test - 31

--- test/soma.spec.ts
+++
@@ -1,6 +0,0 @@
-describe('Dummy Test Suite', () => {
-    it('should pass without any assertions', () => {
-      // This test does not contain any assertions, so it will always pass.
-      // It's just a dummy test to demonstrate a simple test setup.
-    });
-  });
